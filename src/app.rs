a620705d9e3d23e3f535030fc380b29d2f9d3aa9
name <sarkeshnandu@gmail.com>
2024-02-18 23:20:11 +0530

    Still figuring out how to add css to the Leptos based wasm ap

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,11 @@
 use leptos::*;
 
 #[component]
-pub fn App(initial_value:i32) -> impl IntoView{
+pub fn App(initial_value: i32) -> impl IntoView {
     let (counter, set_counter) = create_signal(initial_value);
     view! {
         <div>
-            <button on:click = move |_| set_counter.set(0)>"Clear"</button><br/>
+            <button class="clear-button" on:click = move |_| set_counter.set(0)>"Clear"</button><br/>
             <button on:click = move |_| set_counter.update(|counter| *counter += 1)>"Increment"</button><br/>
             <span>"Counter: " {counter} </span><br/>
             <button on:click = move |_| set_counter.update(|counter| *counter -= 1)>"Decrement"</button>
